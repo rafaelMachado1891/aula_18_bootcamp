5d107a08eabfefd35b0c715b74046e3386062a2a
rafaelMachado1891 <rafadmn1@gmail.com>
2024-08-13 21:59:45 -0300

    subindo meus arquivos

---
+++ backend/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean
+from sqlalchemy.sql import func
+from database import Base
+
+class ProductModel(Base):
+    __tablename__ = "obras"
+
+    id = Column(Integer, primary_key=True)
+    filial = Column(String)
+    postes = Column(Boolean)
+    ligacao = Column(Boolean)
+    inauguracao = Column(String)
+    obra = Column(String)
+    responsavel = (String)
+    link = (String)
+    projetos = (String)
+    orcamento =(String)
+    material =(String)
+    ti = (Boolean)
+    equipe = (String)
+    orcamento_final = (String)
+    status = (String)
+    created = Column(DateTime(timezone=True),default=func.now())
